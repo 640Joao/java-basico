32f066e8ff7028d10ede566fce4d60d29d2c4b8e
João Elias <joanegreiros15@gmail.com>
2025-03-24 16:26:23 -0300

    novo commit para meu projeto ContaBanco

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[]args){
-        System.out.println("Testando");
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("--------------------\nCriação de Conta Bancária\n--------------------");
+
+        System.out.print("Digite o número: ");
+        numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("\nDigite a agência: ");
+        agencia = scanner.nextLine();
+
+        System.out.print("\nDigite o seu nome completo: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.print("\nDigite o valor de depósito: ");
+        saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+         + ", conta " + numero + " e seu saldo é " + saldo + " já disponível para saque");
     }
 }
